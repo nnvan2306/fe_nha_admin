d52de2a7e898a3f497ddd8839866f6f42d833a4b
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2024-04-09 23:50:28 +0700

    update

--- src/systems/pages/Rating/CreateRating/CreateRating.jsx
+++ src/systems/pages/Rating/CreateRating/CreateRating.jsx
@@ -69,6 +69,7 @@ export default function CreateRating() {
             }
         };
         fetch();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const reSet = () => {
