528971437d86faf9f0c49aed41ec068e0effe537
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2024-04-26 23:41:31 +0700

    updtae

--- src/systems/pages/Setting/Setting.jsx
+++ src/systems/pages/Setting/Setting.jsx
@@ -1,3 +1,22 @@
+import classNames from "classnames/bind";
+import styles from "./Setting.module.scss";
+
+const cx = classNames.bind(styles);
+
 export default function Setting() {
-    return <div>Setting</div>;
+    return (
+        <div className={cx("form-setting")}>
+            <div className={cx("form-avatar")}>
+                <img src="" alt="" />
+            </div>
+            <div className={cx("form-info")}>
+                <div className={cx("form-input")}>
+                    <label htmlFor="">Name</label>
+                    <br />
+                    <input type="text" />
+                    <i className="bi bi-pencil"></i>
+                </div>
+            </div>
+        </div>
+    );
 }
